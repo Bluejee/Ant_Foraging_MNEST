055632d500eaad605e0c2056f87e9b628f77f51f
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-04-21 17:11:36 +0530

    Added random search parameter list creation.

--- Parallel_Processing/Parameter_Estimation.py
+++ Parallel_Processing/Parameter_Estimation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import csv
 
 
 def script_maker():
@@ -10,7 +11,7 @@ def script_maker():
                     f"python Ants.py --sim_name=Test_4_disp_{round(disp, 4)}_D_{round(dcy, 4)} -ns --dispersion_rate={round(disp, 4)} --decay_rate={round(dcy, 4)}\n")
 
 
-def parallel_dict_maker():
+def parallel_dict_maker(list_type='Grid'):
     parameter_dict = {'dispersion_rate': [],
                       'decay_rate': [],
                       'drop_amount': [],
@@ -19,28 +20,54 @@ def parallel_dict_maker():
                       'exploration_decay': [],
                       'learning_rate': [],
                       'discounted_return': []}
-
-    for disp in np.arange(0.0, 0.11, 0.01):
-        for dcy in np.arange(0.0, 0.011, 0.001):
-            for drop_amount in [0.05]:
-                for min_exp in [0.05]:
-                    for exp_rate in [0.9]:
-                        for exp_dcy in [0.0001]:
-                            for lern_rate in [0.4]:
-                                for disc_ret in [0.85]:
-                                    parameter_dict['dispersion_rate'].append(disp)
-                                    parameter_dict['decay_rate'].append(dcy)
-                                    parameter_dict['drop_amount'].append(drop_amount)
-                                    parameter_dict['min_exploration'].append(min_exp)
-                                    parameter_dict['exploration_rate'].append(exp_rate)
-                                    parameter_dict['exploration_decay'].append(exp_dcy)
-                                    parameter_dict['learning_rate'].append(lern_rate)
-                                    parameter_dict['discounted_return'].append(disc_ret)
+    if list_type == 'Random':
+        num_tests = 10
+        for index in range(num_tests):
+            parameter_dict['dispersion_rate'].append(np.random.random())
+            parameter_dict['decay_rate'].append(np.random.random())
+            parameter_dict['drop_amount'].append(np.random.random())
+            parameter_dict['min_exploration'].append(np.random.random())
+            parameter_dict['exploration_rate'].append(np.random.random())
+            parameter_dict['exploration_decay'].append(np.random.random())
+            parameter_dict['learning_rate'].append(np.random.random())
+            parameter_dict['discounted_return'].append(np.random.random())
+        pass
+    elif list_type == 'Grid':
+        for disp in np.arange(0.0, 0.11, 0.01):
+            for dcy in np.arange(0.0, 0.011, 0.001):
+                for drop_amount in [0.05]:
+                    for min_exp in [0.05]:
+                        for exp_rate in [0.9]:
+                            for exp_dcy in [0.0001]:
+                                for lern_rate in [0.4]:
+                                    for disc_ret in [0.85]:
+                                        parameter_dict['dispersion_rate'].append(disp)
+                                        parameter_dict['decay_rate'].append(dcy)
+                                        parameter_dict['drop_amount'].append(drop_amount)
+                                        parameter_dict['min_exploration'].append(min_exp)
+                                        parameter_dict['exploration_rate'].append(exp_rate)
+                                        parameter_dict['exploration_decay'].append(exp_dcy)
+                                        parameter_dict['learning_rate'].append(lern_rate)
+                                        parameter_dict['discounted_return'].append(disc_ret)
+    else:
+        print('Wrong Type.')
 
     print(len(parameter_dict['decay_rate']))
     with open('parameter_dict.pickle', 'wb') as f:
         # Use pickle.dump() to write the dictionary to the file
         pickle.dump(parameter_dict, f)
 
+    with open('parameter_list.csv', 'w') as f:
+        # Create a writer object
+        writer = csv.writer(f)
 
-parallel_dict_maker()
+        # Write the header row
+        writer.writerow(parameter_dict.keys())
+
+        # Write the data rows
+        for i in range(len(parameter_dict['decay_rate'])):
+            row = [parameter_dict[name][i] for name in parameter_dict]
+            writer.writerow(row)
+
+
+parallel_dict_maker('Random')
